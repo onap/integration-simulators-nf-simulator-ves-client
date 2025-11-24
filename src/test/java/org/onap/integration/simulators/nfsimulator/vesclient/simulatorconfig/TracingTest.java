617cbdcaa7d430072ddfb095016a2126510b1e15
Fiete Ostkamp <fiete.ostkamp@telekom.de>
2025-11-24 14:36:39 +0100

    Add tracing to ves-client
    - add tracing related dependencies and config
    - make logback directory configurable via LOG_DIR environment
      variable (this requires a chart change in
      pythonsdk-tests/src/onaptests/templates/artifacts/pnf-simulator.tar.gz)
    - bump version to 1.1.0-SNAPSHOT
    
    Issue-ID: INT-2335
    Change-Id: I475b6a1221d1ab33712725cffaf4310f9bb89700
    Signed-off-by: Fiete Ostkamp <fiete.ostkamp@telekom.de>

---
+++ src/test/java/org/onap/integration/simulators/nfsimulator/vesclient/simulatorconfig/TracingTest.java
@@ -0,0 +1,78 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Simulator
+ * ================================================================================
+ * Copyright (C) 2025 Deutsche Telekom. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.integration.simulators.nfsimulator.vesclient.simulatorconfig;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.awaitility.Awaitility.await;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.onap.integration.simulators.nfsimulator.vesclient.Main;
+import org.onap.integration.simulators.nfsimulator.vesclient.simulator.SimulatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.concurrent.TimeUnit;
+
+// @DirtiesContext is needed because the SimulatorService mock bean
+// would replace the real bean beyond the test context of this class otherwise
+@DirtiesContext
+@SpringBootTest(classes = {Main.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 9411)
+@TestPropertySource(properties = {
+    "spring.zipkin.base-url=http://localhost:${wiremock.server.port}",
+    "spring.sleuth.enabled=true",
+    "spring.sleuth.sampler.probability=1.0",
+})
+public class TracingTest {
+
+    @MockBean
+    private SimulatorService simulatorService;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldExportTraceWhenMakingRequest() throws InterruptedException {
+        WireMock.stubFor(post(urlEqualTo("/api/v2/spans"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.ACCEPTED.value())));
+
+        restTemplate.getForEntity("/simulator/config", String.class);
+
+        Thread.sleep(2000);
+        WireMock.verify(WireMock.postRequestedFor(WireMock.urlEqualTo("/api/v2/spans")));
+        // await()
+        //     .atMost(20, TimeUnit.SECONDS)
+        //     .untilAsserted(() -> WireMock.verify(postRequestedFor(urlEqualTo("/api/v2/spans"))));
+    }
+}
